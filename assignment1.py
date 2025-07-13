a7e7300c5cdb12e2691d36ebe1bee982c72bc8fc
unknown <kashishavani@gmail.com>
2025-07-13 13:36:43 +0530

    initial commit

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+hours = input("enter hours: ")
+rate = input("enter rate: ")
+
+#convert values to float
+hours = float(hours)
+rate = float(rate)
+
+#define and print pay
+pay = hours * rate
+print("your pay is:", pay)
